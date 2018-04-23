7bd246496a223d3bbd3964a1c07df8a5cf98acce
webmastir <webmastir@gmail.com>
2018-04-23 15:22:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Worlddd!");
+    response.end("Hello World from Jenkins + Docker!");
 
 });
 
